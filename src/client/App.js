b198ba500f87280f3bb9731e16575da09b38a755
Laney McGlohon <lmcglohon@gmail.com>
2022-02-04 07:19:24 -0800

    Allow new posts to be submitted but not persisted

--- src/client/App.js
+++ src/client/App.js
@@ -1,5 +1,6 @@
 /* eslint-disable react/prefer-stateless-function */
 import React, { Component } from 'react';
+import '../../assets/css/style.css';
 
 const posts = [{
   id: 2,
@@ -20,14 +21,38 @@ const posts = [{
 
 export default class App extends Component {
   state = {
-    posts: posts
+    posts: posts,
+    postContent: ''
   }
-
+  handlePostContentChange = (event) => {
+    this.setState({postContent: event.target.value})
+  }
+  handleSubmit = (event) => {
+    event.preventDefault();
+    const newPost = {
+        id: this.state.posts.length + 1,
+        text: this.state.postContent,
+        user: {
+            avatar: '/uploads/avatar3.png',
+            username: 'Fake User'
+        }
+    };
+    this.setState((prevState) => ({
+        posts: [newPost, ...prevState.posts],
+        postContent: ''
+    }));
+  }
   render() {
     const { posts, postContent } = this.state;
 
     return (
       <div className="container">
+        <div className="postForm">
+          <form onSubmit={this.handleSubmit}>
+            <textarea value={postContent} onChange={this.handlePostContentChange} placeholder="Write your custom post!"/>
+            <input type="submit" value="Submit" />
+          </form>
+        </div>
         <div className="feed">
           {posts.map((post, i) => (
             <div key={post.id} className="post">
